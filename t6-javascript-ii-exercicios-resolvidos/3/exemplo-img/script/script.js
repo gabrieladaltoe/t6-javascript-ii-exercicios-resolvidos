db2113a658885bf2159c87a3a6d9b8823ff4fb28
Gabriela Dal Toé Fortuna <gabrieladaltoe@gmail.com>
2018-09-20 10:04:48 -0300

    Legenda da galeria - JS

--- t6-javascript-ii-exercicios-resolvidos/3/exemplo-img/script/script.js
+++ t6-javascript-ii-exercicios-resolvidos/3/exemplo-img/script/script.js
@@ -1,5 +1,6 @@
 const links = document.querySelector(".gallery__nav")
 const imagem = document.querySelector(".gallery__image")
+const figCaption = document.querySelector(".gallery__caption")
 
 //console.log(links)
 //console.log(links.children) >> children pega a tag e childNode pega todos os nós.
@@ -12,8 +13,26 @@ for (let i = 0; i < links.children.length; i++){
         //pegar o caminho da img no atributo data-image
         const img = this.dataset.image;
         console.log(img)
-
         imagem.src = img
+
+        const descricao = this.dataset.title;
+        console.log(descricao)
+        figCaption.innerHTML = descricao
 
     })
 }
+
+/* for (let i = 0; i < links.children.length; i++){
+    // console.log(links.children[i]
+ 
+     const fio = links.children[i]
+     fio.addEventListener("click", function(){
+         //pegar o caminho da img no atributo data-image
+         const descricao = this.dataset.title;
+         console.log(descricao)
+ 
+         figCaption.innerHTML = descricao
+ 
+     })
+ } */
+
